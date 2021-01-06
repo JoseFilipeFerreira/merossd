fb6a6c62e5f7d7a0b8c59bf27ced0bbcf10ec3d9
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2021-01-06 13:48:45 +0000

    reload info if exception

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
-import asyncio 
+import asyncio
 from quart import Quart
 from meross import MerossWrapper
 
 app = Quart(__name__)
 light = MerossWrapper()
+
+
+async def reload_if_exception(f):
+    try:
+        return await f()
+    except Exception as e:
+        print(e)
+        global light
+        light = MerossWrapper()
+        return str(e)
 
 @app.route('/bulb/')
 async def status():
@@ -11,15 +21,15 @@ async def status():
 
 @app.route('/bulb/on')
 async def on():
-    return await light.on()
+    return await reload_if_exception(lambda: light.on())
 
 @app.route('/bulb/off')
 async def off():
-    return await light.off()
+    return await reload_if_exception(lambda: light.off())
 
 @app.route('/bulb/toggle')
 async def toggle():
-    return await light.toggle()
+    return await reload_if_exception(lambda: light.toggle())
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(light.connect())
